77b04f579f684629e0f0ac9aa7298bac6e29e4d6
Melanie Misty418 Ducani <melanieducani@gmail.com>
2016-04-12 15:09:03 +0200

    buffer: getWaveForm clean

--- src/buffer.js
+++ src/buffer.js
@@ -68,33 +68,31 @@ WalContext.Buffer.prototype = {
 			leftChan = this.getPeaks( 0, width ),
 			rightChan = this.getPeaks( 1, width ),
 			canvas = document.createElement( "canvas" ),
-			canvasCtx = canvas.getContext('2d'),
+			canvasCtx = canvas.getContext( "2d" ),
 			m = height / 2
 		;
 
 		canvas.width = width;
 		canvas.height = height;
-		if ( !colorBack && !colorWave )
-			return null;
-		if ( colorBack && colorWave) {
+		if ( colorBack && colorWave ) {
 			canvasCtx.fillStyle = colorBack;
 			canvasCtx.fillRect( 0, 0, width, height );
 		}
-		canvasCtx.strokeStyle = colorWave || colorBack;
-		canvasCtx.lineCap = "round";
-		canvasCtx.beginPath();
-		for ( var i = 0; i < width; i++ ) {
-			var l = m * ( 1 - leftChan[ i ] );
-			var r = m * ( 1 + rightChan[ i ] );
-			canvasCtx.moveTo( i, colorWave ? l : height );
-			canvasCtx.lineTo( i, r );
-			if ( !colorWave ) {
-				canvasCtx.moveTo( i, l );
-				canvasCtx.lineTo( i, 0 );
+		if ( colorBack || colorWave ) {
+			canvasCtx.strokeStyle = colorWave || colorBack;
+			canvasCtx.beginPath();
+			for ( var i = 0; i < width; i++ ) {
+				var l = m * ( 1 - leftChan[ i ] );
+				var r = m * ( 1 + rightChan[ i ] );
+				canvasCtx.moveTo( i, colorWave ? l : height );
+				canvasCtx.lineTo( i, r );
+				if ( !colorWave ) {
+					canvasCtx.moveTo( i, l );
+					canvasCtx.lineTo( i, 0 );
+				}
 			}
+			canvasCtx.stroke();
 		}
-		canvasCtx.stroke();
-	
 		return canvas;
 	}
 }
