154c92c3b32b42fbb3c92a41fd8c244018f7bdb3
Melanie Misty418 Ducani <melanieducani@gmail.com>
2016-04-12 05:39:27 +0200

    buffer: getPeaks + getWaveForm clean

--- src/buffer.js
+++ src/buffer.js
@@ -63,51 +63,38 @@ WalContext.Buffer.prototype = {
 		}
 		return peaks;
 	},
-	getWaveForm: function( width, height, colorLine, colorBack ) {
+	getWaveForm: function( width, height, colorWave, colorBack ) {
 		var
-			left,
-			right,
-			canvas,
-			canvasCtx,
-			y0
+			leftChan = this.getPeaks( 0, width ),
+			rightChan = this.getPeaks( 1, width ),
+			canvas = document.createElement( "canvas" ),
+			canvasCtx = canvas.getContext('2d'),
+			m = height / 2
 		;
 
-		left = this.getPeaks( 0, width );
-		right = this.getPeaks( 1, width );
-		canvas = document.createElement( "canvas" );
 		canvas.width = width;
 		canvas.height = height;
-		y0 = height / 2;
-		canvasCtx = canvas.getContext('2d');
-		if ( colorBack ) {
+		if ( !colorBack && !colorWave )
+			return null;
+		if ( colorBack && colorWave) {
 			canvasCtx.fillStyle = colorBack;
-			canvasCtx.strokeStyle = colorBack;
+			canvasCtx.fillRect( 0, 0, width, height );
 		}
-		if ( !colorLine && colorBack ) {
-			canvasCtx.beginPath();		
-			for ( var i = 0; i < width; i++) {
-				canvasCtx.moveTo( i, 0 );
-				canvasCtx.lineTo( i, y0 - ( left[ i ] * y0 ) );
-				canvasCtx.moveTo( i, y0 + ( right[ i ] * y0 ) );
-				canvasCtx.lineTo( i, height );
+		canvasCtx.strokeStyle = colorWave || colorBack;
+		canvasCtx.lineCap = "round";
+		canvasCtx.beginPath();
+		for ( var i = 0; i < width; i++ ) {
+			var l = m * ( 1 - leftChan[ i ] );
+			var r = m * ( 1 + rightChan[ i ] );
+			canvasCtx.moveTo( i, colorWave ? l : height );
+			canvasCtx.lineTo( i, r );
+			if ( !colorWave ) {
+				canvasCtx.moveTo( i, l );
+				canvasCtx.lineTo( i, 0 );
 			}
-			canvasCtx.stroke();
-		} else if ( colorLine ) {
-			if ( colorBack ) {
-				canvasCtx.fillRect(0, 0, width, height);
-				canvasCtx.strokeRect(0, 0, width, height);
-			}
-			console.log("toto");
-			canvasCtx.fillStyle = colorLine;
-			canvasCtx.strokeStyle = colorLine;
-			canvasCtx.lineCap = "round";
-			canvasCtx.beginPath();
-			for ( var i = 0; i < width; i++) {
-				canvasCtx.moveTo( i, y0 - ( left[ i ] * y0 ) );
-				canvasCtx.lineTo( i, y0 + ( right[ i ] * y0 ) );
-			}
-			canvasCtx.stroke();
 		}
+		canvasCtx.stroke();
+	
 		return canvas;
 	}
 }
