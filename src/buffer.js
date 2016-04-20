4202a0438d62bccbc602ca8a5ca2fa061958b731
Melanie Misty418 Ducani <melanieducani@gmail.com>
2016-04-20 16:27:30 +0200

    buffer.getWaveForm changed to drawWaveForm and doesn't create the canvas anymore.

--- src/buffer.js
+++ src/buffer.js
@@ -63,30 +63,29 @@ walContext.Buffer.prototype = {
 		}
 		return peaks;
 	},
-	getWaveForm: function( width, height, colorWave, colorBack ) {
+	drawWaveForm: function( canvas, colorWave, colorBack ) {
 		var
 			l, r,
 			i = 0,
-			h2 = height / 2,
-			lChan = this.getPeaks( 0, width ),
-			rChan = this.buffer.numberOfChannels > 1 ? this.getPeaks( 1, width ) : lChan,
-			canvas = document.createElement( "canvas" ),
-			canvasCtx = canvas.getContext( "2d" )
+			h = canvas.height,
+			w = canvas.width,
+			h2 = h / 2,
+			canvasCtx = canvas.getContext( "2d" ),
+			lChan = this.getPeaks( 0, w ),
+			rChan = this.buffer.numberOfChannels > 1 ? this.getPeaks( 1, w ) : lChan
 		;
 
-		canvas.width = width;
-		canvas.height = height;
 		if ( colorBack && colorWave ) {
 			canvasCtx.fillStyle = colorBack;
-			canvasCtx.fillRect( 0, 0, width, height );
+			canvasCtx.fillRect( 0, 0, w, h );
 		}
 		if ( colorBack || colorWave ) {
 			canvasCtx.strokeStyle = colorWave || colorBack;
 			canvasCtx.beginPath();
-			for ( ; i < width; ++i ) {
+			for ( ; i < w; ++i ) {
 				l = h2 * ( 1 - lChan[ i ] );
 				r = h2 * ( 1 + rChan[ i ] );
-				canvasCtx.moveTo( i, colorWave ? l : height );
+				canvasCtx.moveTo( i, colorWave ? l : h );
 				canvasCtx.lineTo( i, r );
 				if ( !colorWave ) {
 					canvasCtx.moveTo( i, l );
