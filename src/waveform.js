1b426454fa0d624685c3db0cdb651c3842d82b60
Thomas Tortorini <thomastortorini@gmail.com>
2016-07-16 06:26:59 +0200

    waveform : SVG

--- src/waveform.js
+++ src/waveform.js
@@ -1,6 +1,31 @@
 "use strict";
 
-(function() {
+( function() {
+
+walContext.Buffer.prototype.waveformSVG = function( svg, w, h ) {
+	var x,
+		h2a = h / 2 - .5,
+		h2b = h / 2 + .5,
+		d = "M0 " + h2a,
+		path = svg ? svg.firstChild : document.createElement( "path" ),
+		lChan = this.getPeaks( 0, w ),
+		rChan = this.buffer.numberOfChannels > 1 ? this.getPeaks( 1, w ) : lChan;
+
+	if ( !svg ) {
+		svg = document.createElement( "svg" );
+		svg.appendChild( path );
+	}
+
+	for ( x = 0; x < w; ++x ) {
+		d += " L" + x + " " + ( h2a - lChan[ x ] * h2a );
+	}
+	for ( x = w - 1; x >= 0; --x ) {
+		d += " L" + x + " " + ( h2b + rChan[ x ] * h2a );
+	}
+	path.setAttribute( "d", d );
+	svg.setAttribute( "viewBox", "0 0 " + w + " " + h );
+	return svg;
+};
 
 function colorPix( data, index, color ) {
 	data[ index ] = color[ 0 ];    
@@ -10,19 +35,16 @@ function colorPix( data, index, color ) {
 }
 
 function waveform( wbuf, canvasImg, color, inverted ) {
-	var
-		y, ymin, ymax,
+	var x, y, ymin, ymax,
 		data = canvasImg.data,
-		x = 0,
 		w = canvasImg.width,
 		h = canvasImg.height,
 		h2 = h / 2,
 		lChan = wbuf.getPeaks( 0, w ),
-		rChan = wbuf.buffer.numberOfChannels > 1 ? wbuf.getPeaks( 1, w ) : lChan
-	;
+		rChan = wbuf.buffer.numberOfChannels > 1 ? wbuf.getPeaks( 1, w ) : lChan;
 
 	if ( inverted ) {
-		for ( ; x < data.length; x += 4 ) {
+		for ( x = 0; x < data.length; x += 4 ) {
 			colorPix( data, x, color );
 		}
 		color = [ 0, 0, 0, 0 ];
@@ -47,4 +69,4 @@ walContext.Buffer.prototype.drawInvertedWaveform = function( canvasImg, color ) 
 	return waveform( this, canvasImg, color, true );
 };
 
-})();
+} )();
