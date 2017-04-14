adbc2f7ab13019a8c2c434d8043ad4f0c3841ba2
Thomas Tortorini <thomastortorini@gmail.com>
2017-04-14 03:49:51 +0200

    gswaBufferSample: setData/load/unload

--- src/gswaBufferSample.js
+++ src/gswaBufferSample.js
@@ -10,38 +10,28 @@ gswaBufferSample.prototype = {
 	},
 	setMetadata: function( obj ) {
 		if ( obj ) {
-			this.duration = this.duration || obj.duration;
+			if ( obj.name ) { this.name = obj.name; }
+			if ( obj.filename ) { this.filename = obj.filename; }
+			if ( Number.isFinite( obj.duration ) ) { this.duration = obj.duration; }
 		}
 	},
-	dropData: function() {
+	setData: function( data ) {
+		this.data = data;
+	},
+	load: function() {
+		var dat = this.data;
+
+		return (
+			dat instanceof AudioBuffer ? Promise.resolve( this._setDataFromAudioBuffer( dat ) ) :
+			dat instanceof ArrayBuffer ? this._setDataFromArrayBuffer( dat ) :
+			dat instanceof Blob ? this._setDataFromBlob( dat ) :
+			this._setDataFromURL( dat )
+		);
+	},
+	unload: function() {
 		this.stop();
 		this.disconnect();
 		this.buffer = null;
-	},
-	setDataFromAudioBuffer: function( audioBuffer ) {
-		this.buffer = audioBuffer;
-		this.setMetadata( audioBuffer );
-		return audioBuffer;
-	},
-	setDataFromArrayBuffer: function( arrayBuffer ) {
-		return this.ctx.decodeAudioData( arrayBuffer )
-			.then( this.setDataFromAudioBuffer.bind( this ) );
-	},
-	setDataFromBlob: function( blob ) {
-		var that = this,
-			reader = new FileReader();
-
-		return new Promise( function( resolve, reject ) {
-			reader.onloadend = function() {
-				resolve( that.setDataFromArrayBuffer( reader.result ) );
-			};
-			reader.readAsArrayBuffer( blob );
-		} );
-	},
-	setDataFromURL: function( url ) {
-		return fetch( url )
-			.then( function( res ) { return res.arrayBuffer(); } )
-			.then( this.setDataFromArrayBuffer.bind( this ) );
 	},
 	connect: function( node ) {
 		this.connectedTo = node;
@@ -81,5 +71,30 @@ gswaBufferSample.prototype = {
 	_removeSource: function( bSource ) {
 		this.bufferSources.splice(
 			this.bufferSources.indexOf( bSource ), 1 );
+	},
+	_setDataFromAudioBuffer: function( audioBuffer ) {
+		this.buffer = audioBuffer;
+		this.duration = audioBuffer.duration;
+		return audioBuffer;
+	},
+	_setDataFromArrayBuffer: function( arrayBuffer ) {
+		return this.ctx.decodeAudioData( arrayBuffer )
+			.then( this._setDataFromAudioBuffer.bind( this ) );
+	},
+	_setDataFromBlob: function( blob ) {
+		var that = this,
+			reader = new FileReader();
+
+		return new Promise( function( resolve, reject ) {
+			reader.onloadend = function() {
+				resolve( that._setDataFromArrayBuffer( reader.result ) );
+			};
+			reader.readAsArrayBuffer( blob );
+		} );
+	},
+	_setDataFromURL: function( url ) {
+		return fetch( url )
+			.then( function( res ) { return res.arrayBuffer(); } )
+			.then( this._setDataFromArrayBuffer.bind( this ) );
 	}
 };
