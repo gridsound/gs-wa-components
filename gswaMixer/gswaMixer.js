7a1203b077de9d531290b700aec170bce17a8c11
Thomas Tortorini <thomastortorini@gmail.com>
2019-08-31 03:42:25 +0200

    gswaMixer: use of gsdataMixer for state logic

--- gswaMixer/gswaMixer.js
+++ gswaMixer/gswaMixer.js
@@ -2,43 +2,54 @@
 
 class gswaMixer {
 	constructor() {
+		this.ctx =
+		this.connectedTo = null;
 		this._chans = {};
 		this._fftSize = 128;
 		this.audioDataL = new Uint8Array( 64 );
 		this.audioDataR = new Uint8Array( 64 );
-		this.data = this._proxInit();
+		this.gsdata = new gsdataMixer( {
+			addChan: this._addChan.bind( this ),
+			removeChan: this._removeChan.bind( this ),
+			toggleChan: this._updateChanToggle.bind( this ),
+			redirectChan: this._updateChanDest.bind( this ),
+			changePanChan: this._updateChanPan.bind( this ),
+			changeGainChan: this._updateChanGain.bind( this ),
+		} );
+		Object.seal( this );
 	}
 
 	setContext( ctx ) {
-		const data = this.data,
-			dataEnt = Object.entries( data );
-
 		this.disconnect();
 		this.ctx = ctx;
-		if ( dataEnt.length ) {
-			dataEnt.forEach( kv => delete data[ kv[ 0 ] ] );
-			dataEnt.forEach( kv => data[ kv[ 0 ] ] = kv[ 1 ] );
+		if ( this.gsdata.values.get( "nbChannels" ) > 0 ) {
+			this.gsdata.reset();
 		} else {
-			data.main = {
-				order: 0,
-				toggle: true,
-				name: "",
-				pan: 0,
-				gain: 1,
-			};
+			this.gsdata.change( {
+				main: {
+					toggle: true,
+					name: "main",
+					gain: 1,
+					pan: 0,
+				},
+			} );
 		}
+	}
+	change( obj ) {
+		this.gsdata.change( obj );
 	}
 	clear() {
 		const d = this.data;
 
-		Object.keys( d ).forEach( id => {
-			if ( id !== "main" ) {
-				delete d[ id ];
-			}
+		this.gsdata.clear();
+		this.gsdata.change( {
+			main: {
+				toggle: true,
+				name: "main",
+				gain: 1,
+				pan: 0,
+			},
 		} );
-		d.main.pan = 0;
-		d.main.gain = 1;
-		d.main.toggle = true;
 	}
 	connect( dest ) {
 		this.disconnect();
@@ -61,7 +72,7 @@ class gswaMixer {
 		nodes.analyserR.getByteFrequencyData( this.audioDataR );
 	}
 	liveUpdateChan( chanId, prop, val ) {
-		this._updateChan( chanId, prop, val );
+		this.gsdata.liveChange( chanId, prop, val );
 	}
 
 	// chan:
@@ -89,8 +100,28 @@ class gswaMixer {
 			input, pan, gain, output, splitter, analyserL, analyserR,
 			analyserData: new Uint8Array( analyserL.frequencyBinCount )
 		};
+		Object.entries( this.gsdata.data ).forEach( kv => {
+			if ( kv[ 1 ].dest === id ) {
+				this._updateChan( kv[ 0 ], "dest", id );
+			}
+		} );
 	}
-	_deleteChan( id ) {
+	_updateChanPan( id, val ) {
+		this._chans[ id ].pan.setValueAtTime( val, this.ctx.currentTime );
+	}
+	_updateChanGain( id, val ) {
+		this._chans[ id ].gain.gain.setValueAtTime( val, this.ctx.currentTime );
+	}
+	_updateChanToggle( id, val ) {
+		this._chans[ id ].gain.gain.setValueAtTime( val ? this.gsdata.data[ id ].gain : 0, this.ctx.currentTime );
+	}
+	_updateChanDest( id, val ) {
+		this._chans[ id ].output.disconnect();
+		if ( val in this.gsdata.data ) {
+			this._chans[ id ].output.connect( this._chans[ val ].input );
+		}
+	}
+	_removeChan( id ) {
 		const nodes = this._chans[ id ];
 
 		nodes.pan.disconnect();
@@ -99,86 +130,5 @@ class gswaMixer {
 		nodes.output.disconnect();
 		nodes.splitter.disconnect();
 		delete this._chans[ id ];
-	}
-	_updateChan( id, prop, val ) {
-		const nodes = this._chans[ id ],
-			now = this.ctx.currentTime;
-
-		switch ( prop ) {
-			case "pan":
-				nodes.pan.setValueAtTime( val, now );
-				break;
-			case "gain":
-				nodes.gain.gain.setValueAtTime( val, now );
-				break;
-			case "toggle":
-				nodes.gain.gain.setValueAtTime( val ? this.data[ id ].gain : 0, now );
-				break;
-			case "dest":
-				nodes.output.disconnect();
-				if ( val in this.data ) {
-					nodes.output.connect( this._chans[ val ].input );
-				}
-				break;
-		}
-	}
-
-	// proxy:
-	_proxInit() {
-		return new Proxy( {}, {
-			set: this._proxAddChan.bind( this ),
-			deleteProperty: this._proxDeleteChan.bind( this ),
-		} );
-	}
-	_proxAddChan( tar, prop, val ) {
-		this._proxDeleteChan( tar, prop );
-		return this.__proxAddChan( tar, prop, val );
-	}
-	_proxDeleteChan( tar, prop ) {
-		if ( prop in tar ) {
-			this._deleteChan( prop );
-			delete tar[ prop ];
-		}
-		return true;
-	}
-	_proxUpdateChan( id, tar, prop, val ) {
-		tar[ prop ] = val;
-		this._updateChan( id, prop, val );
-		return true;
-	}
-	__proxNewChan( id ) {
-		const ch = {
-				toggle: true,
-				order: 0,
-				name: "",
-				gain: 0,
-				pan: 0,
-			};
-
-		if ( id !== "main" ) {
-			ch.dest = "main";
-		}
-		return Object.seal( ch );
-	}
-	__proxAddChan( tar, id, obj ) {
-		const updateChan = this._proxUpdateChan.bind( this, id ),
-			chan = new Proxy( Object.seal( this.__proxNewChan( id ) ), { set: updateChan } );
-
-		tar[ id ] = chan;
-		this._addChan( id );
-		chan.pan = obj.pan;
-		chan.gain = obj.gain;
-		chan.name = obj.name;
-		chan.toggle = obj.toggle;
-		chan.order = obj.order;
-		if ( obj.dest ) {
-			chan.dest = obj.dest;
-		}
-		Object.values( this.data ).forEach( chan => {
-			if ( chan.dest === id ) {
-				chan.dest = id;
-			}
-		} );
-		return true;
 	}
 }
