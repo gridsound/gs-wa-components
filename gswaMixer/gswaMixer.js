3bbde85976f57e504816dc17ff421d2a346520e8
Thomas Tortorini <thomastortorini@gmail.com>
2019-10-10 17:55:13 +0200

    gswaMixer: getChanOutput

--- gswaMixer/gswaMixer.js
+++ gswaMixer/gswaMixer.js
@@ -65,6 +65,9 @@ class gswaMixer {
 	getChanInput( id ) {
 		return this._chans[ id ].input;
 	}
+	getChanOutput( id ) {
+		return this._chans[ id ].pan.getInput();
+	}
 	fillAudioData( chanId ) {
 		const nodes = this._chans[ chanId ];
 
