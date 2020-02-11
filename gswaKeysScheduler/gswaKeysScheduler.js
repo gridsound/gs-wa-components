d79e1442608ae7cada7315b2d82c62d9ba0c9233
Thomas Tortorini <thomastortorini@gmail.com>
2020-02-11 22:26:54 +0100

    gswaKeysScheduler: endlines

--- gswaKeysScheduler/gswaKeysScheduler.js
+++ gswaKeysScheduler/gswaKeysScheduler.js
@@ -9,9 +9,9 @@ class gswaKeysScheduler {
 
 		this.scheduler.currentTime = () => ctx.currentTime;
 		this.scheduler.ondatastart = this._onstartKey.bind( this );
-		this.scheduler.ondatastop	= this._onstopKey.bind( this );
+		this.scheduler.ondatastop = this._onstopKey.bind( this );
 		this.scheduler.enableStreaming( !( ctx instanceof OfflineAudioContext ) );
-  }
+	}
 
 	setSynth( synth ) {
 		this._synth = synth;
@@ -25,10 +25,11 @@ class gswaKeysScheduler {
 	stop() {
 		this.scheduler.stop();
 	}
+
 	_onstartKey( startedId, blcs, when, off, dur ) {
 		this._startedKeys.set( startedId,
 			this._synth.startKey( blcs, when, off, dur ) );
-		}
+	}
 	_onstopKey( startedId ) {
 		this._synth.stopKey( this._startedKeys.get( startedId ) );
 		this._startedKeys.delete( startedId );
