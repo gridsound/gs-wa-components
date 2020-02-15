3e5641ec0a700d1e436143ea45dca550028f62ed
Thomas Tortorini <thomastortorini@gmail.com>
2020-02-15 19:55:04 +0100

    gswaDrumsScheduler

---
+++ gswaDrumsScheduler/gswaDrumsScheduler.js
@@ -0,0 +1,47 @@
+"use strict";
+
+class gswaDrumsScheduler {
+	constructor( ctx ) {
+		this.scheduler = new gswaScheduler();
+		this._drumrows = null;
+		this._startedDrums = new Map();
+		Object.seal( this );
+
+		this.scheduler.currentTime = () => ctx.currentTime;
+		this.scheduler.ondatastart = this._onstartDrum.bind( this );
+		this.scheduler.ondatastop = this._onstopDrum.bind( this );
+		this.scheduler.enableStreaming( !( ctx instanceof OfflineAudioContext ) );
+	}
+
+	setDrumrows( drumrows ) {
+		this._drumrows = drumrows;
+	}
+	change( obj ) {
+		const cpy = DAWCore.utils.deepCopy( obj );
+
+		Object.values( cpy ).forEach( drum => {
+			const when = drum && drum.when;
+
+			if ( when !== undefined ) {
+				drum.offset = 0;
+				drum.duration = this._drumrows.getPatternDurationByRowId( drum.row );
+			}
+		} );
+		DAWCore.utils.diffAssign( this.scheduler.data, cpy );
+	}
+	start( when, off, dur ) {
+		this.scheduler.start( when, off, dur );
+	}
+	stop() {
+		this.scheduler.stop();
+	}
+
+	_onstartDrum( startedId, [ drum ], when, off, dur ) {
+		this._startedDrums.set( startedId,
+			this._drumrows.startDrum( drum[ 1 ], when, off, dur ) );
+	}
+	_onstopDrum( startedId ) {
+		this._drumrows.stopDrum( this._startedDrums.get( startedId ) );
+		this._startedDrums.delete( startedId );
+	}
+}
