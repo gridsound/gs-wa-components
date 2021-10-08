1c53c8e0ed7887ca1b23e3175120d30d0e1a2bb4
mr21 <thomastortorini@gmail.com>
2021-10-08 10:13:06 +0200

    gswaStereoPanner: private fields

--- gswaStereoPanner/gswaStereoPanner.js
+++ gswaStereoPanner/gswaStereoPanner.js
@@ -1,31 +1,37 @@
 "use strict";
 
 class gswaStereoPanner {
+	#splitter = null
+	#left = null
+	#right = null
+	#merger = null
+
 	constructor( ctx ) {
-		this._splitter = ctx.createChannelSplitter( 2 );
-		this._left = ctx.createGain();
-		this._right = ctx.createGain();
-		this._merger = ctx.createChannelMerger( 2 );
-		this._splitter.connect( this._left, 0 );
-		this._splitter.connect( this._right, 1 );
-		this._left.connect( this._merger, 0, 0 );
-		this._right.connect( this._merger, 0, 1 );
+		Object.freeze( this );
+		this.#splitter = ctx.createChannelSplitter( 2 );
+		this.#left = ctx.createGain();
+		this.#right = ctx.createGain();
+		this.#merger = ctx.createChannelMerger( 2 );
+		this.#splitter.connect( this.#left, 0 );
+		this.#splitter.connect( this.#right, 1 );
+		this.#left.connect( this.#merger, 0, 0 );
+		this.#right.connect( this.#merger, 0, 1 );
 	}
 
 	connect( ...args ) {
-		return this._merger.connect( ...args );
+		return this.#merger.connect( ...args );
 	}
 	disconnect( ...args ) {
-		return this._merger.disconnect( ...args );
+		return this.#merger.disconnect( ...args );
 	}
 	getInput() {
-		return this._splitter;
+		return this.#splitter;
 	}
 	getValue() {
-		return this._right.gain.value - this._left.gain.value;
+		return this.#right.gain.value - this.#left.gain.value;
 	}
 	setValueAtTime( value, when ) {
-		this._left.gain.setValueAtTime( Math.min( 1 - value, 1 ), when );
-		this._right.gain.setValueAtTime( Math.min( 1 + value, 1 ), when );
+		this.#left.gain.setValueAtTime( Math.min( 1 - value, 1 ), when );
+		this.#right.gain.setValueAtTime( Math.min( 1 + value, 1 ), when );
 	}
 }
