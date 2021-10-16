62f589d9498828b59423e3f7a198f8a8b7f0ce8c
mr21 <thomastortorini@gmail.com>
2021-10-16 04:14:43 +0200

    gswaFxFilter: private fields

--- gswaFxFilter/gswaFxFilter.js
+++ gswaFxFilter/gswaFxFilter.js
@@ -1,25 +1,26 @@
 "use strict";
 
 class gswaFxFilter {
+	ctx = null
+	input = null
+	output = null
+	responseHzIn = null
+	responseMagOut = null
+	responsePhaseOut = null
+	#filter = null
+	#respSize = -1
+	#enable = false
+	#ctrl = new DAWCore.controllersFx.filter( {
+		dataCallbacks: {
+			type: this.#changeType.bind( this ),
+			Q: this.#changeProp.bind( this, "Q" ),
+			gain: this.#changeProp.bind( this, "gain" ),
+			detune: this.#changeProp.bind( this, "detune" ),
+			frequency: this.#changeProp.bind( this, "frequency" ),
+		},
+	} )
+
 	constructor() {
-		this.ctx =
-		this.input =
-		this.output =
-		this._filter =
-		this.responseHzIn =
-		this.responseMagOut =
-		this.responsePhaseOut = null;
-		this._respSize = -1;
-		this._enable = false;
-		this._ctrl = new DAWCore.controllersFx.filter( {
-			dataCallbacks: {
-				type: this._changeType.bind( this ),
-				Q: this._changeProp.bind( this, "Q" ),
-				gain: this._changeProp.bind( this, "gain" ),
-				detune: this._changeProp.bind( this, "detune" ),
-				frequency: this._changeProp.bind( this, "frequency" ),
-			},
-		} );
 		Object.seal( this );
 	}
 
@@ -28,44 +29,44 @@ class gswaFxFilter {
 		if ( this.ctx ) {
 			this.input.disconnect();
 			this.output.disconnect();
-			this._filter.disconnect();
+			this.#filter.disconnect();
 		}
 		this.ctx = ctx;
 		this.input = ctx.createGain();
 		this.output = ctx.createGain();
-		this._filter = ctx.createBiquadFilter();
-		this._ctrl.recall();
-		this.toggle( this._enable );
+		this.#filter = ctx.createBiquadFilter();
+		this.#ctrl.recall();
+		this.toggle( this.#enable );
 	}
 	toggle( b ) {
-		this._enable = b;
+		this.#enable = b;
 		if ( this.ctx ) {
 			if ( b ) {
 				this.input.disconnect();
-				this.input.connect( this._filter );
-				this._filter.connect( this.output );
+				this.input.connect( this.#filter );
+				this.#filter.connect( this.output );
 			} else {
-				this._filter.disconnect();
+				this.#filter.disconnect();
 				this.input.connect( this.output );
 			}
 		}
 	}
 	change( obj ) {
-		this._ctrl.change( obj );
+		this.#ctrl.change( obj );
 	}
 	liveChange( prop, val ) {
-		this._changeProp( prop, val );
+		this.#changeProp( prop, val );
 	}
 	clear() {
-		this._ctrl.clear();
-		this._respSize = -1;
+		this.#ctrl.clear();
+		this.#respSize = -1;
 		this.responseHzIn =
 		this.responseMagOut =
 		this.responsePhaseOut = null;
 	}
 	updateResponse( size ) {
-		this._createResponseArrays( size );
-		this._filter.getFrequencyResponse(
+		this.#createResponseArrays( size );
+		this.#filter.getFrequencyResponse(
 			this.responseHzIn,
 			this.responseMagOut,
 			this.responsePhaseOut );
@@ -73,18 +74,18 @@ class gswaFxFilter {
 	}
 
 	// .........................................................................
-	_changeType( type ) {
-		this._filter.type = type;
+	#changeType( type ) {
+		this.#filter.type = type;
 	}
-	_changeProp( prop, val ) {
-		this._filter[ prop ].setValueAtTime( val, this.ctx.currentTime );
+	#changeProp( prop, val ) {
+		this.#filter[ prop ].setValueAtTime( val, this.ctx.currentTime );
 	}
-	_createResponseArrays( w ) {
-		if ( w !== this._respSize ) {
+	#createResponseArrays( w ) {
+		if ( w !== this.#respSize ) {
 			const nyquist = this.ctx.sampleRate / 2,
 				Hz = new Float32Array( w );
 
-			this._respSize = w;
+			this.#respSize = w;
 			this.responseHzIn = Hz;
 			this.responseMagOut = new Float32Array( w );
 			this.responsePhaseOut = new Float32Array( w );
