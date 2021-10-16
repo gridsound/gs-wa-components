f1132e3ac8b68d482b2d96365214b00ff78ee683
mr21 <thomastortorini@gmail.com>
2021-10-16 04:02:49 +0200

    gswaPeriodicWaves: class + private fields

--- gswaPeriodicWaves/gswaPeriodicWaves.js
+++ gswaPeriodicWaves/gswaPeriodicWaves.js
@@ -1,21 +1,21 @@
 "use strict";
 
-const gswaPeriodicWaves = Object.freeze( {
-	list: new Map(),
-	_cache: new Map(),
+class gswaPeriodicWaves {
+	static list = new Map()
+	static #cache = new Map()
 
-	clearCache() {
-		this._cache.clear();
-	},
-	get( ctx, name ) {
-		let p = this._cache.get( name );
+	static clearCache() {
+		gswaPeriodicWaves.#cache.clear();
+	}
+	static get( ctx, name ) {
+		let p = gswaPeriodicWaves.#cache.get( name );
 
 		if ( !p ) {
 			const w = gswaPeriodicWaves.list.get( name );
 
 			p = ctx.createPeriodicWave( w.real, w.imag );
-			this._cache.set( name, p );
+			gswaPeriodicWaves.#cache.set( name, p );
 		}
 		return p;
-	},
-} );
+	}
+}
