983a61511d819547011d83349b03211bcaf75319
mr21 <thomastortorini@gmail.com>
2021-10-16 04:07:40 +0200

    gswaBPMTap: class + private fields

--- gswaBPMTap/gswaBPMTap.js
+++ gswaBPMTap/gswaBPMTap.js
@@ -1,22 +1,22 @@
 "use strict";
 
-const gswaBPMTap = {
-	_stack: [],
-	_timeBefore: 0,
-	_stackLimit: 10,
+class gswaBPMTap {
+	static #stack = []
+	static #timeBefore = 0
+	static #stackLimit = 10
 
-	reset() {
-		this._timeBefore =
-		this._stack.length = 0;
-	},
-	tap() {
+	static reset() {
+		gswaBPMTap.#timeBefore =
+		gswaBPMTap.#stack.length = 0;
+	}
+	static tap() {
 		const time = Date.now(),
-			timeBefore = this._timeBefore;
+			timeBefore = gswaBPMTap.#timeBefore;
 
-		this._timeBefore = time;
+		gswaBPMTap.#timeBefore = time;
 		if ( timeBefore ) {
 			const bpm = 60000 / ( time - timeBefore ),
-				stack = this._stack,
+				stack = gswaBPMTap.#stack,
 				lastBpm = stack.length
 					? stack[ stack.length - 1 ]
 					: 0;
@@ -24,12 +24,12 @@ const gswaBPMTap = {
 			if ( lastBpm && ( bpm < lastBpm / 1.5 || bpm > lastBpm * 1.5 ) ) {
 				stack.length = 0;
 			} else {
-				if ( stack.unshift( bpm ) > this._stackLimit ) {
-					stack.length = this._stackLimit;
+				if ( stack.unshift( bpm ) > gswaBPMTap.#stackLimit ) {
+					stack.length = gswaBPMTap.#stackLimit;
 				}
 				return stack.reduce( ( sum, bpm ) => sum + bpm, 0 ) / stack.length;
 			}
 		}
 		return 0;
-	},
-};
+	}
+}
