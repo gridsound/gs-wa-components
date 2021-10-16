3db7648bd46ff432a342cf6e61b003be3c673685
mr21 <thomastortorini@gmail.com>
2021-10-16 04:16:18 +0200

    gswaDrumrows: private fields

--- gswaDrumrows/gswaDrumrows.js
+++ gswaDrumrows/gswaDrumrows.js
@@ -1,23 +1,23 @@
 "use strict";
 
 class gswaDrumrows {
-	constructor() {
-		const ctrl = new DAWCore.controllers.drumrows( {
-				dataCallbacks: {
-					addDrumrow: DAWCore.utils.noop,
-					removeDrumrow: this._removeDrumrow.bind( this ),
-					changeDrumrow: this._changeDrumrow.bind( this ),
-				},
-			} );
+	static #startedMaxId = 0
+	ctx = null
+	onstartdrum = null
+	onstartdrumcut = null
+	getAudioBuffer = DAWCore.utils.noop
+	getChannelInput = DAWCore.utils.noop
+	#bps = 1
+	#startedDrums = new Map()
+	#ctrl = new DAWCore.controllers.drumrows( {
+		dataCallbacks: {
+			addDrumrow: DAWCore.utils.noop,
+			removeDrumrow: this.#removeDrumrow.bind( this ),
+			changeDrumrow: this.#changeDrumrow.bind( this ),
+		},
+	} )
 
-		this.ctx =
-		this.onstartdrum =
-		this.onstartdrumcut = null;
-		this.getAudioBuffer =
-		this.getChannelInput = DAWCore.utils.noop;
-		this._startedDrums = new Map();
-		this._bps = 1;
-		this._ctrl = ctrl;
+	constructor() {
 		Object.seal( this );
 	}
 
@@ -27,21 +27,20 @@ class gswaDrumrows {
 		this.ctx = ctx;
 	}
 	setBPM( bpm ) {
-		this._bps = bpm / 60;
+		this.#bps = bpm / 60;
 	}
 	change( obj ) {
-		this._ctrl.change( obj );
+		this.#ctrl.change( obj );
 	}
 	clear() {
-		this._ctrl.clear();
+		this.#ctrl.clear();
 	}
 	getPatternDurationByRowId( rowId ) {
-		const d = this._ctrl.data;
+		const d = this.#ctrl.data;
 
 		return d.patterns[ d.drumrows[ rowId ].pattern ].duration;
 	}
 
-	// start/stop
 	// .........................................................................
 	startLiveDrum( rowId ) {
 		const drum = {
@@ -51,10 +50,10 @@ class gswaDrumrows {
 				pan: 0,
 			};
 
-		return this._startDrum( drum, this.ctx.currentTime, 0, null, true );
+		return this.#startDrum( drum, this.ctx.currentTime, 0, null, true );
 	}
 	stopLiveDrum( rowId ) {
-		this._startedDrums.forEach( ( nodes, id ) => {
+		this.#startedDrums.forEach( ( nodes, id ) => {
 			// if ( nodes.live && nodes.rowId === rowId ) {
 			if ( nodes.rowId === rowId ) {
 				this.stopDrum( id, "-f" );
@@ -65,7 +64,7 @@ class gswaDrumrows {
 		const cutDur = .001,
 			whenCutStart = when - cutDur;
 
-		this._startedDrums.forEach( ( nodes, id ) => {
+		this.#startedDrums.forEach( ( nodes, id ) => {
 			if ( nodes.absn && nodes.rowId === drumcut.row && nodes.when < whenCutStart && when < nodes.endAt ) {
 				nodes.endAt = when;
 				nodes.gainCut.gain.setValueCurveAtTime( new Float32Array( [ 1, 0 ] ), whenCutStart, cutDur );
@@ -80,16 +79,16 @@ class gswaDrumrows {
 		} );
 	}
 	startDrum( drum, when, off, dur ) {
-		return this._startDrum( drum, when, off, dur, false );
+		return this.#startDrum( drum, when, off, dur, false );
 	}
-	_startDrum( drum, when, off, durUser, live ) {
-		const data = this._ctrl.data,
+	#startDrum( drum, when, off, durUser, live ) {
+		const data = this.#ctrl.data,
 			rowId = drum.row,
 			row = data.drumrows[ rowId ],
 			pat = data.patterns[ row.pattern ],
 			buffer = this.getAudioBuffer( pat.buffer ),
 			dur = durUser !== null ? durUser : buffer ? buffer.duration : 0,
-			id = ++gswaDrumrows._startedMaxId.value,
+			id = ++gswaDrumrows.#startedMaxId,
 			nodes = {
 				rowId, live, when, dur,
 				endAt: when + dur,
@@ -121,29 +120,29 @@ class gswaDrumrows {
 				nodes.startDrumTimeoutId = setTimeout( () => this.onstartdrum( rowId ), timeoutMs );
 			}
 		}
-		this._startedDrums.set( id, nodes );
-		this._startedDrums.forEach( ( nodes, id ) => {
+		this.#startedDrums.set( id, nodes );
+		this.#startedDrums.forEach( ( nodes, id ) => {
 			if ( nodes.when + nodes.dur <= this.ctx.currentTime ) {
-				this._stopDrum( id, nodes );
+				this.#stopDrum( id, nodes );
 			}
 		} );
 		return id;
 	}
 	stopAllDrums() {
-		this._startedDrums.forEach( ( _nodes, id ) => this.stopDrum( id, "-f" ) );
+		this.#startedDrums.forEach( ( nodes, id ) => this.stopDrum( id, "-f" ) );
 	}
 	stopDrum( id, force ) {
-		const nodes = this._startedDrums.get( id );
+		const nodes = this.#startedDrums.get( id );
 
 		if ( nodes && ( force === "-f" ||
 			nodes.when + nodes.dur <= this.ctx.currentTime ||
 			nodes.when >= this.ctx.currentTime
 		) ) {
-			this._stopDrum( id, nodes );
+			this.#stopDrum( id, nodes );
 		}
 	}
-	_stopDrum( id, nodes ) {
-		this._startedDrums.delete( id );
+	#stopDrum( id, nodes ) {
+		this.#startedDrums.delete( id );
 		clearTimeout( nodes.startDrumTimeoutId );
 		clearTimeout( nodes.startDrumcutTimeoutId );
 		if ( nodes.absn ) {
@@ -157,10 +156,10 @@ class gswaDrumrows {
 		}
 	}
 	changeDrumProp( id, prop, val ) {
-		const nodes = this._startedDrums.get( +id );
+		const nodes = this.#startedDrums.get( +id );
 
 		if ( nodes ) {
-			const row = this._ctrl.data.drumrows[ nodes.rowId ];
+			const row = this.#ctrl.data.drumrows[ nodes.rowId ];
 
 			switch ( prop ) {
 				case "detune":
@@ -179,56 +178,53 @@ class gswaDrumrows {
 		}
 	}
 
-	// add/remove/update
 	// .........................................................................
-	_removeDrumrow( id ) {
-		this._startedDrums.forEach( ( nodes, startedId ) => {
+	#removeDrumrow( id ) {
+		this.#startedDrums.forEach( ( nodes, startedId ) => {
 			if ( nodes.rowId === id ) {
 				this.stopDrum( startedId, "-f" );
 			}
 		} );
 	}
-	_changeDrumrow( id, prop, val ) {
-		const row = this._ctrl.data.drumrows[ id ];
+	#changeDrumrow( id, prop, val ) {
+		const row = this.#ctrl.data.drumrows[ id ];
 
 		switch ( prop ) {
 			case "toggle":
-				this.__changeDrumrow( id, nodes => {
+				this.#changeDrumrowProp( id, nodes => {
 					nodes.gainRow.gain.setValueAtTime( val ? row.gain : 0, this.ctx.currentTime );
 				} );
 				break;
 			case "dest":
-				this.__changeDrumrow( id, nodes => {
+				this.#changeDrumrowProp( id, nodes => {
 					nodes.gainRow.disconnect();
 					nodes.gainRow.connect( this.getChannelInput( val ) );
 				} );
 				break;
 			case "detune":
-				this.__changeDrumrow( id, nodes => {
+				this.#changeDrumrowProp( id, nodes => {
 					nodes.absn.detune.setValueAtTime( ( val + nodes.detune ) * 100, this.ctx.currentTime );
 				} );
 				break;
 			case "gain":
-				this.__changeDrumrow( id, nodes => {
+				this.#changeDrumrowProp( id, nodes => {
 					nodes.gainRow.gain.setValueAtTime( val * nodes.gain, this.ctx.currentTime );
 				} );
 				break;
 			case "pan":
-				this.__changeDrumrow( id, nodes => {
+				this.#changeDrumrowProp( id, nodes => {
 					nodes.panRow.pan.setValueAtTime( DAWCore.utils.panningMerge( val, nodes.pan ), this.ctx.currentTime );
 				} );
 				break;
 		}
 	}
-	__changeDrumrow( rowId, fn ) {
-		this._startedDrums.forEach( nodes => {
+	#changeDrumrowProp( rowId, fn ) {
+		this.#startedDrums.forEach( nodes => {
 			if ( nodes.rowId === rowId && nodes.absn ) {
 				fn( nodes );
 			}
 		} );
 	}
 }
-
-gswaDrumrows._startedMaxId = Object.seal( { value: 0 } );
 
 Object.freeze( gswaDrumrows );
