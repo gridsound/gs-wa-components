ab1ea7e37ca0b50fd2319104478d96fe254ac2ee
mr21 <thomastortorini@gmail.com>
2021-10-16 04:09:18 +0200

    gswaDrumsScheduler: private fields

--- gswaDrumsScheduler/gswaDrumsScheduler.js
+++ gswaDrumsScheduler/gswaDrumsScheduler.js
@@ -1,15 +1,16 @@
 "use strict";
 
 class gswaDrumsScheduler {
+	scheduler = new gswaScheduler()
+	#drumrows = null
+	#startedDrums = new Map()
+
 	constructor() {
-		this.scheduler = new gswaScheduler();
-		this._drumrows = null;
-		this._startedDrums = new Map();
 		Object.seal( this );
 
-		this.scheduler.ondatastart = this._onstartDrum.bind( this );
-		this.scheduler.ondatastop = this._onstopDrum.bind( this );
-		this.scheduler.ondatapropchange = this._onchangeDrum.bind( this );
+		this.scheduler.ondatastart = this.#onstartDrum.bind( this );
+		this.scheduler.ondatastop = this.#onstopDrum.bind( this );
+		this.scheduler.ondatapropchange = this.#onchangeDrum.bind( this );
 	}
 
 	setContext( ctx ) {
@@ -17,14 +18,14 @@ class gswaDrumsScheduler {
 		this.scheduler.enableStreaming( !( ctx instanceof OfflineAudioContext ) );
 	}
 	setDrumrows( drumrows ) {
-		this._drumrows = drumrows;
+		this.#drumrows = drumrows;
 	}
 	change( obj ) {
 		const cpy = DAWCore.utils.deepCopy( obj );
 
 		Object.values( cpy ).forEach( drum => {
 			if ( drum && "when" in drum ) { // 1.
-				drum.duration = this._drumrows.getPatternDurationByRowId( drum.row );
+				drum.duration = this.#drumrows.getPatternDurationByRowId( drum.row );
 			}
 		} );
 		this.scheduler.change( cpy );
@@ -36,20 +37,20 @@ class gswaDrumsScheduler {
 		this.scheduler.stop();
 	}
 
-	_onstartDrum( startedId, [ [ , drum ] ], when, off, _dur ) {
+	#onstartDrum( startedId, [ [ , drum ] ], when, off, dur ) {
 		if ( "gain" in drum ) {
-			this._startedDrums.set( startedId,
-				this._drumrows.startDrum( drum, when, off, drum.duration ) );
+			this.#startedDrums.set( startedId,
+				this.#drumrows.startDrum( drum, when, off, drum.duration ) );
 		} else {
-			this._drumrows.startDrumcut( drum, when );
+			this.#drumrows.startDrumcut( drum, when );
 		}
 	}
-	_onstopDrum( startedId ) {
-		this._drumrows.stopDrum( this._startedDrums.get( startedId ) );
-		this._startedDrums.delete( startedId );
+	#onstopDrum( startedId ) {
+		this.#drumrows.stopDrum( this.#startedDrums.get( startedId ) );
+		this.#startedDrums.delete( startedId );
 	}
-	_onchangeDrum( startedId, prop, val ) {
-		this._drumrows.changeDrumProp( startedId, prop, val );
+	#onchangeDrum( startedId, prop, val ) {
+		this.#drumrows.changeDrumProp( startedId, prop, val );
 	}
 }
 
