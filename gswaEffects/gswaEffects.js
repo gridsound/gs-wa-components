3827092c08e24e2f258ba4222b06bedbe0fefe4b
mr21 <thomastortorini@gmail.com>
2021-10-16 04:16:05 +0200

    gswaEffects: private fields

--- gswaEffects/gswaEffects.js
+++ gswaEffects/gswaEffects.js
@@ -1,64 +1,68 @@
 "use strict";
 
 class gswaEffects {
+	ctx = null
+	#wafxs = new Map()
+	#getChanInput = null
+	#getChanOutput = null
+	#ctrl = new DAWCore.controllers.effects( {
+		dataCallbacks: {
+			changeBPM: bpm => this.#wafxs.forEach( fx => fx.setBPM && fx.setBPM( bpm ) ),
+			addEffect: this.#addEffect.bind( this ),
+			removeEffect: this.#removeEffect.bind( this ),
+			changeEffect: this.#changeEffect.bind( this ),
+			connectEffectTo: this.#connectEffectTo.bind( this ),
+			changeEffectData: ( id, data ) => this.getFx( id ).change( data ),
+		},
+	} )
+
 	constructor( fns ) {
-		this.ctx = null;
-		this._getChanInput = fns.getChanInput;
-		this._getChanOutput = fns.getChanOutput;
-		this._wafxs = new Map();
-		this.gsdata = new DAWCore.controllers.effects( {
-			dataCallbacks: {
-				changeBPM: bpm => this._wafxs.forEach( fx => fx.setBPM && fx.setBPM( bpm ) ),
-				addEffect: this._addEffect.bind( this ),
-				removeEffect: this._removeEffect.bind( this ),
-				changeEffect: this._changeEffect.bind( this ),
-				connectEffectTo: this._connectEffectTo.bind( this ),
-				changeEffectData: ( id, data ) => this._wafxs.get( id ).change( data ),
-			},
-		} );
 		Object.seal( this );
+		this.#getChanInput = fns.getChanInput;
+		this.#getChanOutput = fns.getChanOutput;
 	}
 
 	// .........................................................................
+	getFx( id ) {
+		return this.#wafxs.get( id );
+	}
 	setContext( ctx ) {
 		this.ctx = ctx;
-		this.gsdata.reset();
+		this.#ctrl.reset();
 	}
 	change( obj ) {
-		this.gsdata.change( obj );
+		this.#ctrl.change( obj );
 	}
 	clear() {
-		this.gsdata.clear();
+		this.#ctrl.clear();
 	}
 	liveChangeFxProp( id, prop, val ) {
-		this._wafxs.get( id ).liveChange( prop, val );
+		this.getFx( id ).liveChange( prop, val );
 	}
 
 	// .........................................................................
-	_addEffect( id, fx ) {
+	#addEffect( id, fx ) {
 		const wafx = new ( gswaEffects.fxsMap.get( fx.type ) )();
 
-		this._wafxs.set( id, wafx );
+		this.#wafxs.set( id, wafx );
 		wafx.setContext( this.ctx );
 	}
-	_removeEffect( id ) {
-		const wafx = this._wafxs.get( id );
-
-		wafx.output.disconnect();
-		this._wafxs.delete( id );
+	#removeEffect( id ) {
+		this.getFx( id ).output.disconnect();
+		this.#wafxs.delete( id );
 	}
-	_changeEffect( id, prop, val ) {
+	#changeEffect( id, prop, val ) {
 		if ( prop === "toggle" ) {
-			this._wafxs.get( id ).toggle( val );
+			this.getFx( id ).toggle( val );
 		}
 	}
-	_connectEffectTo( chanId, fxId, nextFxId ) {
+	#connectEffectTo( chanId, fxId, nextFxId ) {
 		const dest = nextFxId
-				? this._wafxs.get( nextFxId ).input
-				: this._getChanOutput( chanId ),
+				? this.#wafxs.get( nextFxId ).input
+				: this.#getChanOutput( chanId ),
 			node = fxId
-				? this._wafxs.get( fxId ).output
-				: this._getChanInput( chanId );
+				? this.#wafxs.get( fxId ).output
+				: this.#getChanInput( chanId );
 
 		if ( node ) {
 			node.disconnect();
