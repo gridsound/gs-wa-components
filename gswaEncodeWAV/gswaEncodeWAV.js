db7000b57da6a0e69ae75ca0cc5bf6ee15315e4b
mr21 <thomastortorini@gmail.com>
2021-10-16 04:15:23 +0200

    gswaEncodeWAV: private fields

--- gswaEncodeWAV/gswaEncodeWAV.js
+++ gswaEncodeWAV/gswaEncodeWAV.js
@@ -9,16 +9,16 @@ class gswaEncodeWAV {
 			bytesPerSample = bitsPerSample / 8,
 			bytesPerbloc = nbChannels * bytesPerSample,
 			samples = nbChannels === 2
-				? gswaEncodeWAV._interleave( buffer.getChannelData( 0 ), buffer.getChannelData( 1 ) )
+				? gswaEncodeWAV.#interleave( buffer.getChannelData( 0 ), buffer.getChannelData( 1 ) )
 				: buffer.getChannelData( 0 ),
 			dataSize = samples.length * bytesPerSample,
 			arrBuffer = new ArrayBuffer( 44 + dataSize ),
 			data = new DataView( arrBuffer );
 
-		gswaEncodeWAV._setString( data, 0, "RIFF" );           // FileTypeBlocID(4) : "RIFF"
+		gswaEncodeWAV.#setString( data, 0, "RIFF" );           // FileTypeBlocID(4) : "RIFF"
 		data.setUint32( 4, 36 + dataSize, true );              // FileSize(4)       : headerSize + dataSize - 8
-		gswaEncodeWAV._setString( data, 8, "WAVE" );           // FileFormatID(4)   : "WAVE"
-		gswaEncodeWAV._setString( data, 12, "fmt " );          // FormatBlocID(4)   : "fmt "
+		gswaEncodeWAV.#setString( data, 8, "WAVE" );           // FileFormatID(4)   : "WAVE"
+		gswaEncodeWAV.#setString( data, 12, "fmt " );          // FormatBlocID(4)   : "fmt "
 		data.setUint32( 16, 16, true );                        // BlocSize(4)       : 16
 		data.setUint16( 20, format, true );                    // AudioFormat(2)    : Format du stockage dans le fichier (1: PCM, ...)
 		data.setUint16( 22, nbChannels, true );                // nbChannels(2)     : 1, 2, ..., 6
@@ -26,18 +26,17 @@ class gswaEncodeWAV {
 		data.setUint32( 28, sampleRate * bytesPerbloc, true ); // bytesPerSec(4)    : sampleRate * bytesPerbloc
 		data.setUint16( 32, bytesPerbloc, true );              // bytesPerbloc(2)   : nbChannels * bitsPerSample / 8
 		data.setUint16( 34, bitsPerSample, true );             // bitsPerSample(2)  : 8, 16, 24
-		gswaEncodeWAV._setString( data, 36, "data" );          // DataBlocID(4)     : "data"
+		gswaEncodeWAV.#setString( data, 36, "data" );          // DataBlocID(4)     : "data"
 		data.setUint32( 40, dataSize, true );                  // dataSize(4)       : fileSize - 44
-
 		( format === 1
-			? gswaEncodeWAV._bufToInt16
-			: gswaEncodeWAV._bufToFloat32
+			? gswaEncodeWAV.#bufToInt16
+			: gswaEncodeWAV.#bufToFloat32
 		)( data, 44, samples );
 		return arrBuffer;
 	}
 
-	// private:
-	static _interleave( ldata, rdata ) {
+	// .........................................................................
+	static #interleave( ldata, rdata ) {
 		const len = ldata.length + rdata.length,
 			arr = new Float32Array( len );
 
@@ -47,19 +46,19 @@ class gswaEncodeWAV {
 		}
 		return arr;
 	}
-	static _setString( data, offset, str ) {
+	static #setString( data, offset, str ) {
 		for ( let i = 0; i < str.length; ++i ) {
 			data.setUint8( offset + i, str.charCodeAt( i ) );
 		}
 	}
-	static _bufToInt16( data, offset, samples ) {
+	static #bufToInt16( data, offset, samples ) {
 		for ( let i = 0; i < samples.length; ++i ) {
 			const s = Math.max( -1, Math.min( samples[ i ], 1 ) );
 
 			data.setInt16( offset + i * 2, s < 0 ? s * 0x8000 : s * 0x7FFF, true );
 		}
 	}
-	static _bufToFloat32( data, offset, samples ) {
+	static #bufToFloat32( data, offset, samples ) {
 		for ( let i = 0; i < samples.length; ++i ) {
 			data.setFloat32( offset + i * 4, samples[ i ], true );
 		}
