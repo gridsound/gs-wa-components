c92f673968c100f878148030614c595260d64340
mr21 <thomastortorini@gmail.com>
2023-03-24 07:52:12 +0100

    gswaMIDIControllersManager: no need of DAWCore
    + beautify a little

--- gswaMIDIControllersManager/gswaMIDIControllersManager.js
+++ gswaMIDIControllersManager/gswaMIDIControllersManager.js
@@ -4,9 +4,8 @@ const MIDI_CHANNEL_NOTEON = 0x90;
 const MIDI_CHANNEL_NOTEOFF = 0x80;
 
 class gswaMIDIControllersManager {
-	#dawcore = null;
-	#uiKeys  = null;
-	#MIDIControllersInput  = new Map();
+	#uiKeys = null;
+	#MIDIControllersInput = new Map();
 	#MIDIControllersOutput = new Map();
 
 	$initMidiAccess( midiAccess ) {
@@ -15,23 +14,15 @@ class gswaMIDIControllersManager {
 
 		this.#openAlreadyConnectedControllers( midiAccess );
 	}
-	$setDAWCore( core ) {
-		this.#dawcore = core;
-	}
 	$setPianorollKeys( uiKeys ) {
 		this.#uiKeys = uiKeys;
 	}
 	#openAlreadyConnectedControllers( midiAccess ) {
-		let deviceInput;
-		let deviceOutput;
-
-		for ( let entry of midiAccess.inputs ) {
-			deviceInput = entry[ 1 ];
-			deviceInput.open();
+		for ( const entry of midiAccess.inputs ) {
+			entry[ 1 ].open();
 		}
-		for ( let entry of midiAccess.outputs ) {
-			deviceOutput = entry[ 1 ];
-			deviceOutput.open();
+		for ( const entry of midiAccess.outputs ) {
+			entry[ 1 ].open();
 		}
 	}
 	#onControllerStateChange( event ) {
@@ -42,41 +33,44 @@ class gswaMIDIControllersManager {
 		}
 	}
 	#addController( port, sysexEnabled ) {
-		if ( port.type === "input" && !this.#MIDIControllersInput.has( port.id )) {
-			let ctrler = new gswaMIDIControllerInput( port.id, port.name, port, sysexEnabled );
+		if ( port.type === "input" && !this.#MIDIControllersInput.has( port.id ) ) {
+			const ctrler = new gswaMIDIControllerInput( port.id, port.name, port, sysexEnabled );
 
 			this.#MIDIControllersInput.set( port.id, ctrler );
 			this.$linkToPianoroll( ctrler );
-		} else if ( port.type === "output" && !this.#MIDIControllersOutput.has( port.id )) {
-			let ctrler = new gswaMIDIControllerOutput( port.id, port.name, port, sysexEnabled );
+		} else if ( port.type === "output" && !this.#MIDIControllersOutput.has( port.id ) ) {
+			const ctrler = new gswaMIDIControllerOutput( port.id, port.name, port, sysexEnabled );
 
 			this.#MIDIControllersOutput.set( port.id, ctrler );
 		}
 	}
 	#removeDevice( port ) {
-		let MIDIControllers = port.type === "input"	? this.#MIDIControllersInput
-													: this.#MIDIControllersOutput;
+		const MIDIControllers = port.type === "input"
+			? this.#MIDIControllersInput
+			: this.#MIDIControllersOutput;
 
 		MIDIControllers.delete( port.id );
 	}
+
 	// ----------------- // Linkable functions
 	// These functions are used to associate controllers buttons, keys, etc to actions or instruments
 	$linkToPianoroll( MIDIController ) {
-		MIDIController.$onNoteOnAdd( this.#pianorollLiveKeyPressed.bind( this ));
-		MIDIController.$onNoteOffAdd( this.#pianoRollLiveKeyReleased.bind( this ));
+		MIDIController.$onNoteOnAdd( this.#pianorollLiveKeyPressed.bind( this ) );
+		MIDIController.$onNoteOffAdd( this.#pianoRollLiveKeyReleased.bind( this ) );
 	}
 	$unlinkFromPianoroll( MIDIController ) {
-		MIDIController.$onNoteOnRemove( this.#pianorollLiveKeyPressed.bind( this ));
-		MIDIController.$onNoteOffRemove( this.#pianoRollLiveKeyReleased.bind( this ));
+		MIDIController.$onNoteOnRemove( this.#pianorollLiveKeyPressed.bind( this ) );
+		MIDIController.$onNoteOffRemove( this.#pianoRollLiveKeyReleased.bind( this ) );
 	}
 
 	// ---------------- // Action functions
 	#pianoRollLiveKeyReleased( MIDIControllerData ) {
-		this.#uiKeys.midiKeyUp( MIDIControllerData[1] );
+		this.#uiKeys?.midiKeyUp( MIDIControllerData[ 1 ] );
 	}
 	#pianorollLiveKeyPressed( MIDIControllerData ) {
-		this.#uiKeys.midiKeyDown( MIDIControllerData[1] );
+		this.#uiKeys?.midiKeyDown( MIDIControllerData[ 1 ] );
 	}
+
 	// ---------------- // print
 	#printControllers() {
 		console.log( "Inputs" );
