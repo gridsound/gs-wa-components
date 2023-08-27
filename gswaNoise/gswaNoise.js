285f06c0e3a3cb8871b19cdbf2b4be26a941c529
mr21 <thomastortorini@gmail.com>
2023-08-27 17:07:25 +0200

    gswaNoise: static class to render ABSN of noise

---
+++ gswaNoise/gswaNoise.js
@@ -0,0 +1,31 @@
+"use strict";
+
+class gswaNoise {
+	static #buffer = null;
+
+	// .........................................................................
+	static $initBuffer( ctx ) {
+		const srate = ctx.sampleRate;
+		const bufSize = 1 * srate;
+		const buf = ctx.createBuffer( 1, bufSize, srate );
+		const chan0 = buf.getChannelData( 0 );
+
+		for ( let i = 0; i < bufSize; ++i ) {
+			chan0[ i ] = Math.random() * 2 - 1;
+		}
+		gswaNoise.#buffer = buf;
+	}
+
+	// .........................................................................
+	static $startABSN( ctx, when, dur ) {
+		const absn = ctx.createBufferSource();
+
+		absn.buffer = gswaNoise.#buffer;
+		absn.loop = true;
+		absn.start( when );
+		if ( Number.isFinite( dur ) ) {
+			absn.stop( when + dur );
+		}
+		return absn;
+	}
+}
