451d9b4ec29b9ae0e0ef5798e8c319037eaa67f2
mr21 <thomastortorini@gmail.com>
2023-11-06 02:09:10 +0100

    gswaNoise: stereo

--- gswaNoise/gswaNoise.js
+++ gswaNoise/gswaNoise.js
@@ -7,11 +7,13 @@ class gswaNoise {
 	static $initBuffer( ctx ) {
 		const srate = ctx.sampleRate;
 		const bufSize = 1 * srate;
-		const buf = ctx.createBuffer( 1, bufSize, srate );
+		const buf = ctx.createBuffer( 2, bufSize, srate );
 		const chan0 = buf.getChannelData( 0 );
+		const chan1 = buf.getChannelData( 1 );
 
 		for ( let i = 0; i < bufSize; ++i ) {
 			chan0[ i ] = Math.random() * 2 - 1;
+			chan1[ i ] = Math.random() * 2 - 1;
 		}
 		gswaNoise.#buffer = buf;
 	}
