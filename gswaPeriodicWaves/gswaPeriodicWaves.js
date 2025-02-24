1d365dca66867611e3fe30731a7660834486ffa3
mr21 <thomastortorini@gmail.com>
2025-02-24 12:13:33 +0100

    gswaPeriodicWaves: load custom waves

--- gswaPeriodicWaves/gswaPeriodicWaves.js
+++ gswaPeriodicWaves/gswaPeriodicWaves.js
@@ -14,24 +14,42 @@ class gswaPeriodicWaves {
 			const w = gswaPeriodicWaves.#list.get( name );
 
 			if ( w ) {
-				p = ctx.createPeriodicWave( w.real, w.imag, { disableNormalization: name === "square" } );
+				p = ctx.createPeriodicWave( w.real, w.imag, { disableNormalization: name === "square" || name.startsWith( "custom" ) } );
 				gswaPeriodicWaves.#cache.set( name, p );
 			}
 		}
 		return p;
 	}
+	static $delete( name ) {
+		gswaPeriodicWaves.#list.delete( name );
+		gswaPeriodicWaves.#cache.delete( name );
+	}
 	static $loadWaves( waves ) {
-		waves.forEach( ( [ name, w ] ) => {
-			const imag = w.imag || w.real.map( () => 0 );
-			const real = w.real || w.imag.map( () => 0 );
+		waves.forEach( w => gswaPeriodicWaves.#loadWave( ...w ) );
+		return gswaPeriodicWaves.#list;
+	}
+	static $loadCustom( name, points ) {
+		const curve = GSUsampleDottedCurve( points, 2 ** 11 );
+		const wave = GSUifft( {
+			real: curve.reverse(),
+			imag: GSUnewArray( curve.length, 0 ),
+		} );
 
+		gswaPeriodicWaves.#cache.delete( name );
+		gswaPeriodicWaves.#loadWave( name, wave, "noFirstZero" );
+		return wave;
+	}
+	static #loadWave( name, wave, noFirstZero ) {
+		const imag = wave.imag || wave.real.map( () => 0 );
+		const real = wave.real || wave.imag.map( () => 0 );
+
+		if ( noFirstZero !== "noFirstZero" ) {
 			real[ 0 ] =
 			imag[ 0 ] = 0;
-			gswaPeriodicWaves.#list.set( name, Object.freeze( {
-				real: new Float32Array( real ),
-				imag: new Float32Array( imag ),
-			} ) );
-		} );
-		return gswaPeriodicWaves.#list;
+		}
+		gswaPeriodicWaves.#list.set( name, Object.freeze( {
+			real: new Float32Array( real ),
+			imag: new Float32Array( imag ),
+		} ) );
 	}
 }
