87a943285b9c30db53ab39a387e0d404f81aac12
mr21 <thomastortorini@gmail.com>
2026-03-26 13:02:33 +0100

    gswaFxDelay: organize the const sources

--- gswaFxDelay/gswaFxDelay.js
+++ gswaFxDelay/gswaFxDelay.js
@@ -11,9 +11,11 @@ class gswaFxDelay {
 	#delayGainB = null;
 	#delayPanA = null;
 	#delayPanB = null;
-	#csTime = null;
-	#csGain = null;
-	#csPan = null;
+	#csNodes = {
+		time: null,
+		gain: null,
+		pan: null,
+	};
 	#gainNegPan = null;
 	#enable = false;
 	#data = GSUgetModel( "fx.delay" );
@@ -77,14 +79,13 @@ class gswaFxDelay {
 
 	// .........................................................................
 	#changeProp( prop, val ) {
-		const now = this.#ctx.currentTime;
-
 		this.#data[ prop ] = val;
-		switch ( prop ) {
-			case "time": this.#csTime.offset.setValueAtTime( val / this.#bps, now ); break;
-			case "gain": this.#csGain.offset.setValueAtTime( val, now ); break;
-			case "pan": this.#csPan.offset.setValueAtTime( val, now ); break;
-		}
+		this.#csNodes[ prop ].offset.setValueAtTime( this.#formatValue( prop, val ), this.#ctx.currentTime );
+	}
+	#formatValue( prop, val ) {
+		return prop !== "time"
+			? val
+			: val / this.#bps;
 	}
 	#createNodes( ctx ) {
 		this.#input = GSUaudioGain( ctx );
@@ -97,26 +98,22 @@ class gswaFxDelay {
 		this.#delayB = GSUaudioDelay( ctx, 20 );
 		this.#delayGainA.gain.value = 0;
 		this.#delayGainB.gain.value = 0;
-		this.#delayA.delayTime.value = 0;
-		this.#delayB.delayTime.value = 0;
 		this.#delayPanA.pan.value = 0;
 		this.#delayPanB.pan.value = 0;
+		this.#delayA.delayTime.value = 0;
+		this.#delayB.delayTime.value = 0;
 	}
 	#createConstantSourceLayer( ctx ) {
-		this.#csTime = ctx.createConstantSource();
-		this.#csGain = ctx.createConstantSource();
-		this.#csPan = ctx.createConstantSource();
+		GSUforEach( this.#csNodes, ( _, k, obj ) => obj[ k ] = ctx.createConstantSource() );
 		this.#gainNegPan = ctx.createGain();
 		this.#gainNegPan.gain.value = -1;
-		this.#csTime.connect( this.#delayA.delayTime );
-		this.#csTime.connect( this.#delayB.delayTime );
-		this.#csGain.connect( this.#delayGainA.gain );
-		this.#csGain.connect( this.#delayGainB.gain );
-		this.#csPan.connect( this.#delayPanA.pan );
-		this.#csPan.connect( this.#gainNegPan ).connect( this.#delayPanB.pan );
-		this.#csTime.start();
-		this.#csGain.start();
-		this.#csPan.start();
+		this.#csNodes.time.connect( this.#delayA.delayTime );
+		this.#csNodes.time.connect( this.#delayB.delayTime );
+		this.#csNodes.gain.connect( this.#delayGainA.gain );
+		this.#csNodes.gain.connect( this.#delayGainB.gain );
+		this.#csNodes.pan.connect( this.#delayPanA.pan );
+		this.#csNodes.pan.connect( this.#gainNegPan ).connect( this.#delayPanB.pan );
+		GSUforEach( this.#csNodes, cs => cs.start() );
 	}
 	#disconnect() {
 		this.#input.disconnect();
@@ -127,12 +124,10 @@ class gswaFxDelay {
 		this.#delayGainB.disconnect();
 		this.#delayPanA.disconnect();
 		this.#delayPanB.disconnect();
-		this.#csTime.stop();
-		this.#csGain.stop();
-		this.#csPan.stop();
-		this.#csTime.disconnect();
-		this.#csGain.disconnect();
-		this.#csPan.disconnect();
+		GSUforEach( this.#csNodes, cs => {
+			cs.stop();
+			cs.disconnect();
+		} );
 		this.#gainNegPan.disconnect();
 	}
 }
