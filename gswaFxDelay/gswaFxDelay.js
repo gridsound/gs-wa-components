c2986e96bb3548d406625c2041d6cc6ea53f785e
mr21 <thomastortorini@gmail.com>
2026-03-26 11:46:21 +0100

    gswaFxDelay: reorganize the nodes to have 3 audioparams only

--- gswaFxDelay/gswaFxDelay.js
+++ gswaFxDelay/gswaFxDelay.js
@@ -11,6 +11,10 @@ class gswaFxDelay {
 	#delayGainB = null;
 	#delayPanA = null;
 	#delayPanB = null;
+	#csTime = null;
+	#csGain = null;
+	#csPan = null;
+	#gainNegPan = null;
 	#enable = false;
 	#data = GSUgetModel( "fx.delay" );
 
@@ -31,24 +35,11 @@ class gswaFxDelay {
 	}
 	$setContext( ctx ) {
 		if ( this.#ctx ) {
-			this.#input.disconnect();
-			this.#output.disconnect();
-			this.#delayA.disconnect();
-			this.#delayB.disconnect();
-			this.#delayGainA.disconnect();
-			this.#delayGainB.disconnect();
-			this.#delayPanA.disconnect();
-			this.#delayPanB.disconnect();
+			this.#disconnect();
 		}
 		this.#ctx = ctx;
-		this.#input = GSUaudioGain( ctx );
-		this.#output = GSUaudioGain( ctx );
-		this.#delayGainA = GSUaudioGain( ctx );
-		this.#delayGainB = GSUaudioGain( ctx );
-		this.#delayPanA = GSUaudioStereoPanner( ctx );
-		this.#delayPanB = GSUaudioStereoPanner( ctx );
-		this.#delayA = GSUaudioDelay( ctx, 20 );
-		this.#delayB = GSUaudioDelay( ctx, 20 );
+		this.#createNodes( ctx );
+		this.#createConstantSourceLayer( ctx );
 		this.$toggle( this.#enable );
 		this.$change( this.#data );
 	}
@@ -86,21 +77,63 @@ class gswaFxDelay {
 
 	// .........................................................................
 	#changeProp( prop, val ) {
+		const now = this.#ctx.currentTime;
+
 		this.#data[ prop ] = val;
 		switch ( prop ) {
-			case "time":
-				this.#delayA.delayTime.setValueAtTime( val / this.#bps, this.#ctx.currentTime );
-				this.#delayB.delayTime.setValueAtTime( val / this.#bps, this.#ctx.currentTime );
-				break;
-			case "gain":
-				this.#delayGainA.gain.setValueAtTime( val, this.#ctx.currentTime );
-				this.#delayGainB.gain.setValueAtTime( val, this.#ctx.currentTime );
-				break;
-			case "pan":
-				this.#delayPanA.pan.setValueAtTime( val, this.#ctx.currentTime );
-				this.#delayPanB.pan.setValueAtTime( -val, this.#ctx.currentTime );
-				break;
+			case "time": this.#csTime.offset.setValueAtTime( val / this.#bps, now ); break;
+			case "gain": this.#csGain.offset.setValueAtTime( val, now ); break;
+			case "pan": this.#csPan.offset.setValueAtTime( val, now ); break;
 		}
+	}
+	#createNodes( ctx ) {
+		this.#input = GSUaudioGain( ctx );
+		this.#output = GSUaudioGain( ctx );
+		this.#delayGainA = GSUaudioGain( ctx );
+		this.#delayGainB = GSUaudioGain( ctx );
+		this.#delayPanA = GSUaudioStereoPanner( ctx );
+		this.#delayPanB = GSUaudioStereoPanner( ctx );
+		this.#delayA = GSUaudioDelay( ctx, 20 );
+		this.#delayB = GSUaudioDelay( ctx, 20 );
+		this.#delayGainA.gain.value = 0;
+		this.#delayGainB.gain.value = 0;
+		this.#delayA.delayTime.value = 0;
+		this.#delayB.delayTime.value = 0;
+		this.#delayPanA.pan.value = 0;
+		this.#delayPanB.pan.value = 0;
+	}
+	#createConstantSourceLayer( ctx ) {
+		this.#csTime = ctx.createConstantSource();
+		this.#csGain = ctx.createConstantSource();
+		this.#csPan = ctx.createConstantSource();
+		this.#gainNegPan = ctx.createGain();
+		this.#gainNegPan.gain.value = -1;
+		this.#csTime.connect( this.#delayA.delayTime );
+		this.#csTime.connect( this.#delayB.delayTime );
+		this.#csGain.connect( this.#delayGainA.gain );
+		this.#csGain.connect( this.#delayGainB.gain );
+		this.#csPan.connect( this.#delayPanA.pan );
+		this.#csPan.connect( this.#gainNegPan ).connect( this.#delayPanB.pan );
+		this.#csTime.start();
+		this.#csGain.start();
+		this.#csPan.start();
+	}
+	#disconnect() {
+		this.#input.disconnect();
+		this.#output.disconnect();
+		this.#delayA.disconnect();
+		this.#delayB.disconnect();
+		this.#delayGainA.disconnect();
+		this.#delayGainB.disconnect();
+		this.#delayPanA.disconnect();
+		this.#delayPanB.disconnect();
+		this.#csTime.stop();
+		this.#csGain.stop();
+		this.#csPan.stop();
+		this.#csTime.disconnect();
+		this.#csGain.disconnect();
+		this.#csPan.disconnect();
+		this.#gainNegPan.disconnect();
 	}
 }
 
